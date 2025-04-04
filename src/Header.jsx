2201f3b648a13d2bc60114ed3a758dd50c539c0e
Juan Portal <juanportal@Portals-Mac-mini.local>
2025-04-03 19:34:03 -0700

    kinda structured

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Navbar } from './Navbar'
+
 
 export const Header = () => {
   return (
     <header>
-        <a href="#"><img src="src/assets/favicon.png" alt="logo" title='logo'/></a>
-        <h1>Figeroa Fotography</h1>
+        <div className='title'>
+            <a href="#"><img src="src/assets/favicon.png" alt="logo" title='logo'/></a>
+            <h1>Figeroa Fotography</h1>
+        </div>
+        <Navbar />
     </header>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <ul>
+        <a href="#">About</a>
+        <a href="#">Gallery</a>
+        <a href="#">Contact</a>
+    </ul>
+  )
+}
